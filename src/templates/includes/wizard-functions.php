4bf49d6176c086ad968851acbeddbb8eea224a3a
mikechalmers <mikechalmers@gmail.com>
2022-12-03 17:18:26 +0000

    add remove dashboard metaboxes function

--- src/templates/includes/wizard-functions.php
+++ src/templates/includes/wizard-functions.php
@@ -20,3 +20,19 @@ function ww_dashboard_help() {
     </div>
     ';
 }
+
+//
+/* remove the unnecessary dashboard meta boxes */
+//
+function remove_dashboard_meta() {
+remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+}
+add_action( 'admin_init', 'remove_dashboard_meta' );
